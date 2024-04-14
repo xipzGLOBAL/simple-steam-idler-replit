e9e15fe0ee2dad990d537b8089cf8ed41fa3cc50
xipzGLOBAL <leonardorocha.com@gmail.com>
2024-04-14 14:14:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
-var username = process.env.username;
-var password = process.env.password;
+var xipz013 = process.env.username;
+var LeuuRocha13! = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [730, 440, 570];  // Enter here AppIDs of the needed games
